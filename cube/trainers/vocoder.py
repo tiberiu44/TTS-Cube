52876221cdc83354874e909a0801d6bab2fa1903
Tiberiu Boros <tibi@racai.ro>
2018-10-28 22:58:30 +0200

    BeeCoder seems to converge

--- cube/trainers/vocoder.py
+++ cube/trainers/vocoder.py
@@ -109,11 +109,11 @@ class Trainer:
                 sys.stdout.write(' avg loss=' + str(loss) + " execution time=" + str(stop - start))
                 sys.stdout.write('\n')
                 sys.stdout.flush()
-                if file_index % 50 == 0:
+                if file_index % 200 == 0:
                      self.synth_devset(batch_size, target_sample_rate)
-                     #self.vocoder.store('data/models/nn_vocoder')
+                     self.vocoder.store('data/models/nn_vocoder')
 
             self.synth_devset(batch_size, target_sample_rate)
-            #self.vocoder.store('data/models/nn_vocoder')
+            self.vocoder.store('data/models/nn_vocoder')
 
             epoch += 1
