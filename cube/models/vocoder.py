b14f009dc3494166b0069563fa8d516a9161fb37
Tiberiu Boros <tibi@racai.ro>
2019-01-15 19:49:48 +0200

    Sync

--- cube/models/vocoder.py
+++ cube/models/vocoder.py
@@ -17,10 +17,48 @@ import torch
 import tqdm
 import numpy as np
 from models.clarinet.wavenet import Wavenet
-from models.clarinet.modules import GaussianLoss
+from models.clarinet.modules import GaussianLoss, STFT, KL_Loss
+from models.clarinet.wavenet_iaf import Wavenet_Student
+from torch.distributions.normal import Normal
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
+
+def _create_batches(y_target, mgc, batch_size):
+    x_list = []
+    y_list = []
+    c_list = []
+
+    x_mini_list = []
+    y_mini_list = []
+    c_mini_list = []
+    mini_batch = 25
+
+    for batch_index in range((len(mgc) - 1) // mini_batch):
+        mgc_start = batch_index * mini_batch
+        mgc_stop = batch_index * mini_batch + mini_batch
+        c_mini_list.append(mgc[mgc_start:mgc_stop].reshape(mini_batch, self.params.mgc_order).transpose())
+        x_start = batch_index * mini_batch * self.UPSAMPLE_COUNT
+        x_stop = batch_index * mini_batch * self.UPSAMPLE_COUNT + mini_batch * self.UPSAMPLE_COUNT
+        x_mini_list.append(y_target[x_start:x_stop].reshape(1, x_stop - x_start))
+        y_mini_list.append(y_target[x_start:x_stop].reshape(x_stop - x_start, 1))
+
+        if len(c_mini_list) == batch_size:
+            x_list.append(torch.tensor(x_mini_list).to(device))
+            y_list.append(torch.tensor(y_mini_list).to(device))
+            c_list.append(torch.tensor(c_mini_list).to(device))
+            c_mini_list = []
+            x_mini_list = []
+            y_mini_list = []
+
+    if len(c_mini_list) != 0:
+        x_list.append(torch.tensor(x_mini_list).to(device))
+        y_list.append(torch.tensor(y_mini_list).to(device))
+        c_list.append(torch.tensor(c_mini_list).to(device))
+    # from ipdb import set_trace
+    # set_trace()
+
+    return x_list, y_list, c_list
 
 class Vocoder:
     def __init__(self, params):
@@ -43,45 +81,11 @@ class Vocoder:
 
         self.trainer = torch.optim.Adam(self.model.parameters(), lr=self.params.learning_rate)
 
-    def _create_batches(self, y_target, mgc, batch_size):
-        x_list = []
-        y_list = []
-        c_list = []
 
-        x_mini_list = []
-        y_mini_list = []
-        c_mini_list = []
-        mini_batch = 25
-
-        for batch_index in range((len(mgc) - 1) // mini_batch):
-            mgc_start = batch_index * mini_batch
-            mgc_stop = batch_index * mini_batch + mini_batch
-            c_mini_list.append(mgc[mgc_start:mgc_stop].reshape( mini_batch, self.params.mgc_order).transpose())
-            x_start = batch_index * mini_batch * self.UPSAMPLE_COUNT
-            x_stop = batch_index * mini_batch * self.UPSAMPLE_COUNT + mini_batch * self.UPSAMPLE_COUNT
-            x_mini_list.append(y_target[x_start:x_stop].reshape(1, x_stop - x_start))
-            y_mini_list.append(y_target[x_start:x_stop].reshape(x_stop - x_start, 1))
-
-            if len(c_mini_list) == batch_size:
-                x_list.append(torch.tensor(x_mini_list).to(device))
-                y_list.append(torch.tensor(y_mini_list).to(device))
-                c_list.append(torch.tensor(c_mini_list).to(device))
-                c_mini_list = []
-                x_mini_list = []
-                y_mini_list = []
-
-        if len(c_mini_list) != 0:
-            x_list.append(torch.tensor(x_mini_list).to(device))
-            y_list.append(torch.tensor(y_mini_list).to(device))
-            c_list.append(torch.tensor(c_mini_list).to(device))
-        # from ipdb import set_trace
-        # set_trace()
-
-        return x_list, y_list, c_list
 
     def learn(self, y_target, mgc, batch_size):
         # prepare batches
-        x_list, y_list, c_list = self._create_batches(y_target, mgc, batch_size)
+        x_list, y_list, c_list = _create_batches(y_target, mgc, batch_size)
         if len(x_list) == 0:
             return 0
         # learn
@@ -105,8 +109,8 @@ class Vocoder:
 
     def synthesize(self, mgc, batch_size):
         num_samples = len(mgc) * self.UPSAMPLE_COUNT
-        #from ipdb import set_trace
-        #set_trace()
+        # from ipdb import set_trace
+        # set_trace()
         with torch.no_grad():
             c = torch.tensor(mgc.transpose(), dtype=torch.float32).to(device).reshape(1, mgc[0].shape[0], len(mgc))
             x = self.model.generate(num_samples - 1, c, device=device)
@@ -130,17 +134,61 @@ class Vocoder:
 
 
 class ParallelVocoder:
-    def __init__(self, wavenet=None):
+    def __init__(self, params, wavenet=None):
+        self.params = params
+        self.model_t = wavenet
+        self.model_s = Wavenet_Student(num_blocks_student=[1, 1, 1, 4],
+                                       num_layers=6)
+
+        self.stft = STFT(filter_length=1024, hop_length=256)
+        self.criterion_t=KL_Loss()
+        self.criterion_frame = torch.nn.MSELoss()
+
+    def learn(self, y_target, mgc, batch_size):
+        # prepare batches
+        x_list, y_list, c_list = _create_batches(y_target, mgc, batch_size)
+        if len(x_list) == 0:
+            return 0
+        # learn
+        total_loss = 0
+        for x, y, c in tqdm.tqdm(zip(x_list, y_list, c_list), total=len(c_list)):
+            x = torch.tensor(x, dtype=torch.float32).to(device)
+            y = torch.tensor(y, dtype=torch.float32).to(device)
+            c = torch.tensor(c, dtype=torch.float32).to(device)
+            self.trainer.zero_grad()
+            q_0 = Normal(x.new_zeros(x.size()), x.new_ones(x.size()))
+            z = q_0.sample()
+            c_up = self.model_t.upsample(c)
+            x_student, mu_s, logs_s = self.model_s(z, c_up)
+            mu_logs_t = self.model_t(x_student, c)
+
+            loss_t, loss_KL, loss_reg = self.criterion_t(mu_s, logs_s, mu_logs_t[:, 0:1, :-1], mu_logs_t[:, 1:, :-1])
+            stft_student, _ = self.stft(x_student[:, :, 1:])
+            stft_truth, _ = self.stft(x[:, :, 1:])
+            loss_frame = self.criterion_frame(stft_student, stft_truth)
+            loss_tot = loss_t + loss_frame
+            total_loss += loss_tot.item()
+            loss_tot.backward()
+
+            torch.nn.utils.clip_grad_norm_(self.model.parameters(), 10.)
+            self.trainer.step()
+
+        return total_loss / len(x_list)
+
+
+    def synthesize(self, mgc, batch_size):
         pass
 
-    def learn(self):
-        pass
+    def save(self, output_base):
+        torch.save(self.model_s.state_dict(), output_base + ".network")
 
-    def synthesize(self):
-        pass
-
-    def save(self, filepath):
-        pass
-
-    def load(self, filepath):
-        pass
+    def load(self, output_base):
+        if torch.cuda.is_available():
+            if torch.cuda.device_count() == 1:
+                self.model.load_state_dict(torch.load(output_base + ".network", map_location='cuda:0'))
+            else:
+                self.model.load_state_dict(torch.load(output_base + ".network"))
+        else:
+            self.model.load_state_dict(
+                torch.load(output_base + '.network', map_location=lambda storage, loc: storage))
+        self.model.to(device)
