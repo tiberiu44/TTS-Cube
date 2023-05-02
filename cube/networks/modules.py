b72656ac6d76102df954160ff694937571183332
Tiberiu Boros <boros@adobe.com>
2023-05-02 19:38:46 +0300

    Sync

--- cube/networks/modules.py
+++ cube/networks/modules.py
@@ -736,15 +736,7 @@ class CubedallEncoder(nn.Module):
             nn.ELU(),
             EncoderBlock(out_channels=16 * C, stride=strides[3]),
             nn.ELU(),
-            ConvNorm(in_channels=16 * C, out_channels=D, kernel_size=7, padding=3),
-            nn.ELU(),
-            ConvNorm(in_channels=D, out_channels=D, kernel_size=7, padding=3),
-            nn.ELU(),
-            ConvNorm(in_channels=D, out_channels=D, kernel_size=7, padding=3),
-            nn.ELU(),
-            ConvNorm(in_channels=D, out_channels=D, kernel_size=7, padding=3),
-            nn.ELU(),
-            ConvNorm(in_channels=D, out_channels=D, kernel_size=7, padding=3),
+            CausalConv1d(in_channels=16*C, out_channels=D, kernel_size=3)
         )
 
     def forward(self, x):
