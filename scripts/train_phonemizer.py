ebb702d3c42cda7161560f846a47533d0226632f
Tiberiu Boros <boros@adobe.com>
2023-06-14 05:39:22 +0300

    Shufling data

--- scripts/train_phonemizer.py
+++ scripts/train_phonemizer.py
@@ -83,7 +83,8 @@ def _train(params):
     trainloader = DataLoader(trainset,
                              batch_size=params.batch_size,
                              num_workers=params.num_workers,
-                             collate_fn=collate.collate_fn)
+                             collate_fn=collate.collate_fn,
+                             shuffle=True)
     devloader = DataLoader(devset,
                            batch_size=params.batch_size,
                            num_workers=params.num_workers,
